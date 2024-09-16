0c9a1d874155c29a36bc92673a308bfffeb99aef
Dileep kumar <burladileepkumar13@gmail.com>
2024-09-16 12:32:20 +0530

    changes made

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,5 +1,5 @@
 
 //export const API_URL="http://localhost:4000"
 
- export const API_URL="https://backend-restaurant-cvmj.onrender.com"
+ export const API_URL="https://backend-restaurant-5tc1.onrender.com"
  
